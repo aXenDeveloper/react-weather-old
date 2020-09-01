178b306f673f44a0c22fd5e6150557b360fd17bf
aXenDev <ithereplay@gmail.com>
2020-09-01 14:40:07 +0200

    Add rain weather

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   state = {
     value: 'Gdańsk',
     clouds: null,
-
+    temp: 200
   }
 
   handleInputChange = e => {
@@ -21,9 +21,10 @@ export default class App extends React.Component {
       const API = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=40ea5ceef9ec33888dffe518d21b0d28&units=metric`);
       const APIJson = await API.json();
       this.setState({
-        clouds: APIJson.clouds.all
+        clouds: APIJson.clouds.all,
+        temp: APIJson.main.temp
       });
-      console.log(this.state.clouds);
+      console.log(APIJson);
     } catch(err) {
       console.log(err);
     }
@@ -35,7 +36,7 @@ export default class App extends React.Component {
         <Form value={this.state.value} 
         change={this.handleInputChange}
         submit={this.handleSubmitChange} />
-        <Content value={this.state.clouds} />
+        <Content temp={this.state.temp} />
       </>
     )
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Form = props => {
     return (
         <div className="topBar">
             <form onSubmit={props.submit}>
                 <input type='text' onChange={props.change} value={props.value} placeholder='Wpisz miasto' />
-                <button>Test</button>
+                <button><FontAwesomeIcon icon={faSearch} /></button>
             </form>
         </div>
     )
